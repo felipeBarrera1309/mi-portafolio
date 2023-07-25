7609d633096b9cbe442de33d464990e5a4cd4900
Felipe Barrera <felipebarrera.gbp@gmail.com>
2023-07-25 17:44:23 -0500

    changes animation

--- src/modules/briefcase/partials/informationMain.vue
+++ src/modules/briefcase/partials/informationMain.vue
@@ -60,7 +60,7 @@
         </div>
         <div
             class="w-64 h-80 bg-cover m-auto
-                bg-[url('/home/felipe/Documentos/cursos-youtube/mi-portafolio/public/generals/photo-felipe.jpg')]
+                bg-[url('/publi/generals/photo-felipe.jpg')]
                 rounded-lg lg:w-full lg:h-full lg:bg-center lg:bg-no-repeat lg:bg-[length:417px_440px]
             "
         />
@@ -74,20 +74,21 @@
         class="my-12 flex gap-4 flex-col w-4/5 m-auto lg:flex-row lg:w-[87%] lg:my-28"
         ref="jobSection"
     >
-        <div class="flex flex-row gap-3 lg:flex-col lg:w-[20%]">
+        <div class="flex flex-row gap-3 md:min-w-[150px] lg:flex-col lg:justify-between lg:w-[20%]">
             <button
                 v-for="(boton, i) in buttonsInformation"
                 :key="i"
                 class="bg-white w-full px-4 py-1
                     border-b-2 border-l-0 transition-action
+                    min-h-[45px]
                     dark:bg-slate-700
                     hover:bg-slate-200
                     hover:dark:bg-slate-200
                     hover:dark:text-black
-                    lg:border-b-0 lg:border-l-2
+                    lg:border-b-0 lg:border-l-2 lg:max-h-[56px]
                     lg:h-full lg:text-left
                 "
-                :class="buttonSelected == i + 1
+                :class="sectionSelected == i + 1
                     ?
                     'border-sky-400 dark:bg-slate-50 dark:text-white bg-slate-200'
                     :
@@ -98,10 +99,34 @@
                 {{ boton.name }}
             </button>
         </div>
-        <article>
-            <h3>Ingerio Electromecanico <span>@Felipe</span></h3>
-            <h4>Yave Felipe Barrera Pico</h4>
-        </article>
+        <div class="my-10 flex lg:my-0 lg:px-10 lg:w-[850px]">
+            <template v-for="(data, i) in personalInformation" :key="i">
+                <article class="open-description text-ellipsis" v-if="sectionSelected == i + 1">
+                    <h3>{{ data.title }} <span class="text-sky-500 font-bold text-lg">{{ data.canal }}</span></h3>
+                    <h4 class="text-sm pb-5">{{ data.subtitle }}</h4>
+                    <ul class="flex flex-col gap-3">
+                        <li class="flex items-center gap-3">
+                            <span class="text-sky-500 font-black pointer-events-none">&#9655</span>
+                            <p class="px-5 font-normal text-stone-900 dark:text-cyan-50">
+                                {{ data.descriptionOne }}
+                            </p>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <span class="text-sky-500 font-black pointer-events-none">&#9655</span>
+                            <p class="px-5 font-normal text-stone-900 dark:text-cyan-50">
+                                {{ data.descriptionTwo }}
+                            </p>
+                        </li>
+                        <li class="flex items-center gap-3">
+                            <span class="text-sky-500 font-black pointer-events-none">&#9655</span>
+                            <p class="px-5 font-normal text-stone-900 dark:text-cyan-50">
+                                {{ data.descriptionThree }}
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+            </template>
+        </div>
     </section>
 </template>
 
@@ -109,21 +134,66 @@
 import { ref } from 'vue';
 
 const jobSection = ref()
-const buttonSelected = ref(1)
+const sectionSelected = ref(1)
 
 const buttonsInformation = ref([
     { name: 'experience' },
     { name: 'experience' },
     { name: 'experience' },
-    { name: 'experience' }
+])
+
+const personalInformation = ref([
+    {
+        title: 'Ingeniero Electromecanico',
+        canal: '@Felipe',
+        subtitle: 'Yave Felipe Barrera Pico',
+        descriptionOne: 'Frontend desarrollador de paginas web',
+        descriptionTwo: 'estoy haciendo mi descripcion numero dos',
+        descriptionThree: 'Estoy haciendo mi descripcion numero tres'
+    },
+    {
+        title: 'FrontEnd',
+        canal: '@Felipe',
+        subtitle: 'Maquetador',
+        descriptionOne: 'Parte de la vista en una pagina web',
+        descriptionTwo: 'estoy haciendo mi descripcion numero dos',
+        descriptionThree: 'Estoy haciendo mi descripcion numero tres'
+    },
+    {
+        title: 'Portafolio FrontEnd',
+        canal: '@Felipe',
+        subtitle: 'decoro mi propio portafolio',
+        descriptionOne: 'Estoy creando mi propio portafolio con el framework vuejs',
+        descriptionTwo: 'estoy haciendo mi descripcion numero dos',
+        descriptionThree: 'Estoy haciendo mi descripcion numero tres Estoy haciendo mi descripcion numero tres Estoy haciendo mi descripcion numero tres'
+    },
 ])
 
 function selectView(selected){
-    buttonSelected.value = selected
+    sectionSelected.value = selected
 }
 
 </script>
 
 <style lang="scss" scoped>
+
+.open-description{
+    width: 100%;
+    animation: open;
+    animation-timing-function: linear;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    transition: all 0.3s ease-in-out;
+}
+
+@keyframes open{
+    0%{
+        width: 0;
+        opacity: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap
+    }
+}
 
 </style>
